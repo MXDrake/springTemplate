684ec4ab5b517baead427d7d00a7c724d2dd88e5
Max <arcas.llc@yandex.ru>
2018-01-28 18:10:24 +0300

    add template

--- src/main/java/main/controller/MainController.java
+++ src/main/java/main/controller/MainController.java
@@ -1,13 +1,17 @@
 package main.controller;
 
+import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
-	public String main() {
-		return "index";
+	public ModelAndView main() throws IOException {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
 }
